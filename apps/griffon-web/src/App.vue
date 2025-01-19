24ed220b14a4eddb5f12c2c2a4c63c4d878fad96
Quentin <rousseltin@gmail.com>
2025-01-19 12:08:28 +0100

    feat(monorepo): init repos front/back, add v1 DrawingBoard component

---
+++ apps/griffon-web/src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import DrawingBoard from './components/DrawingBoard/DrawingBoard.vue';
+</script>
+
+<template>
+<DrawingBoard/>
+</template>
+
---
+++ apps/griffon-web/src/components/DrawingBoard/types.ts
@@ -0,0 +1,11 @@
+export interface DrawingPath {
+  strokeStyle: string
+  lineWidth: number
+  points: PathPoint[]
+  tool: DrawingTool
+}
+interface PathPoint {
+  x: number
+  y: number
+}
+export type DrawingTool = 'brush' | 'eraser'
