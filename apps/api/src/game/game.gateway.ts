b6621396430e2986009d7d44ab7e258bff76f82e
Quentin <rousseltin@gmail.com>
2025-01-20 19:56:47 +0100

    feat(gamegateway): add websocket logic

--- apps/api/src/game/game.gateway.ts
+++ apps/api/src/game/game.gateway.ts
@@ -1,9 +1,45 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
-
+import { Logger } from '@nestjs/common';
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 @WebSocketGateway()
-export class GameGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
+export class GameGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private readonly logger = new Logger(GameGateway.name);
+
+  @WebSocketServer() io: Server;
+
+  afterInit(): void {
+    this.logger.log('Game gateway initialized');
+  }
+
+  handleConnection(client: Socket): void {
+    const { sockets } = this.io.sockets;
+
+    this.logger.log(`Client id: ${client.id} connected`);
+    this.logger.debug(`Number of connected clients: ${sockets.size}`);
+  }
+
+  handleDisconnect(client: Socket): void {
+    this.logger.log(`Cliend id:${client.id} disconnected`);
+  }
+
+  @SubscribeMessage('join-room')
+  handleJoinRoom(
+    @MessageBody('roomId') roomId,
+    @ConnectedSocket() client: Socket,
+  ): string {
+    console.log(client);
+    console.log(roomId);
     return 'Hello world!';
   }
 }
