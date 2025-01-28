8fb716ba076530066ed05eadf5336f98d4f53d56
Quentin <rousseltin@gmail.com>
2025-01-28 23:17:17 +0100

    feat(SocketEventenum): set event enum as package for constistency

---
+++ packages/socket-events/src/socket-events.enum.js
@@ -0,0 +1,11 @@
+const WSE = {
+  INVALID_TOKEN:'invalid-token',
+  USER_JOINED_ROOM: 'user-joined-room',
+  USER_JOINED_ROOM_SUCCESS: 'user-joined-room-success',
+  USER_LEFT_ROOM: 'user-left-room',
+  USER_DISCONNECTED: 'user-disconnected',
+  USER_RECONNECTED: 'user-reconnected',
+  ASK_JOIN_ROOM: 'ask-join-room',
+}
+
+module.exports = {WSE}
