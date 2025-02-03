9c95490dacd7cb66c77b7048e250dd95f39d01b5
Quentin <rousseltin@gmail.com>
2025-02-03 18:17:59 +0100

    feat(wse): improved build process of wse and types

---
+++ packages/socket-events/src/wse.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.WSE = void 0;
+var WSE;
+(function (WSE) {
+    WSE["INVALID_TOKEN"] = "invalid-token";
+    WSE["USER_JOINED_ROOM"] = "user-joined-room";
+    WSE["USER_JOINED_ROOM_SUCCESS"] = "user-joined-room-success";
+    WSE["USER_LEFT_ROOM"] = "user-left-room";
+    WSE["USER_DISCONNECTED"] = "user-disconnected";
+    WSE["USER_RECONNECTED"] = "user-reconnected";
+    WSE["ASK_JOIN_ROOM"] = "ask-join-room";
+    WSE["UPLOAD_DRAWING"] = "upload-drawing";
+})(WSE || (exports.WSE = WSE = {}));
+//# sourceMappingURL=WSE.js.map
---
+++ packages/socket-events/src/wse.ts
@@ -0,0 +1,10 @@
+export enum WSE {
+        INVALID_TOKEN='invalid-token',
+        USER_JOINED_ROOM= 'user-joined-room',
+        USER_JOINED_ROOM_SUCCESS= 'user-joined-room-success',
+        USER_LEFT_ROOM= 'user-left-room',
+        USER_DISCONNECTED= 'user-disconnected',
+        USER_RECONNECTED= 'user-reconnected',
+        ASK_JOIN_ROOM= 'ask-join-room',
+        UPLOAD_DRAWING='upload-drawing'
+}
