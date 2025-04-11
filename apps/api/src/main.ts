9600410c4995d2f83c1f6649f1602ba7fc7dd1a8
Quentin <rousseltin@gmail.com>
2025-04-12 01:42:03 +0200

    fix(api): try fixing api access in production

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,13 +4,13 @@ import { NestFastifyApplication, FastifyAdapter } from '@nestjs/platform-fastify
 import { ConsoleLogger } from '@nestjs/common'
 async function bootstrap(): Promise<void> {
   const fastify = new FastifyAdapter()
-  fastify.enableCors()
   const logger = new ConsoleLogger()
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, fastify, {
     logger,
   })
 
-  await app.listen(process.env.PORT ?? 3000)
+  fastify.enableCors({ origin: '*' })
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0')
   logger.log(process.env.PORT ?? 3000)
 }
 bootstrap()
